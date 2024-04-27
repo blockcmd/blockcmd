8fe0aa0d4c3d86af6cd75cbda347354cfcbb31ea
ZxStim <thang.victor.luong@gmail.com>
2024-04-27 20:42:05 +0700

    added inspect page

--- app/inspect/page.tsx
+++ app/inspect/page.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image"
+
 export default function Page() {
   return (
     <>
+      <Image src="/inspect-banner.png" alt="Inspect banner" width={1200} height={250} />
       <h1>Inspect</h1>
     </>
   )
